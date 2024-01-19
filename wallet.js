3861b70c76a7271dfa4c2238bef3e40e20868923
shariarSultanFahim <157113723+shariarSultanFahim@users.noreply.github.com>
2024-01-19 16:54:19 +0600

    Add files via upload

---
+++ wallet.js
@@ -0,0 +1,52 @@
+function depositeMoney(){
+    var dip = parseInt(document.getElementById('diposite').value); // Getting the value from input field
+    if (document.getElementById('diposite').value.trim() == '' || dip<0){  //This is to ensure that user can't send empty value
+        document.getElementById('diposite').value = '';
+        alert('Please enter a valid number.');
+        return;
+    }
+    var currentAmount = parseInt(document.getElementById('current-wallet').innerText);
+
+    var totalDiposite = parseInt(document.getElementById('totaldeposite').innerText);
+
+    currentAmount += dip; //Adding current and new amount
+    totalDiposite += dip; //Adding total and new amount
+
+    document.getElementById('current-wallet').innerText = currentAmount;
+    document.getElementById('totaldeposite').innerText = totalDiposite;
+    document.getElementById('diposite').value = ''; //Clearing the input field
+}
+
+function withdrawMoney(){
+    var wit = parseInt(document.getElementById('withdraw').value); // Getting the value from input field
+
+    if (document.getElementById('withdraw').value.trim() == '' || wit<0){  //This is to ensure that user can't send empty value
+        document.getElementById('withdraw').value = '';
+        alert('Please enter a valid number.');
+        return;
+    }
+
+    var currentAmount = parseInt(document.getElementById('current-wallet').innerText);
+
+    var totalWithdraw = parseInt(document.getElementById('totalwithdraw').innerText);
+    if(wit > currentAmount){
+            alert('You have not enough money in your Wallet.')
+            document.getElementById('withdraw').value = '';
+        }
+    else{
+        currentAmount -= wit; //Substracting new amount from previous amount
+        totalWithdraw += wit; //Adding previous and new withdraw amount
+        document.getElementById('withdraw').value = ''; //Clearing the input field
+        }
+    
+    document.getElementById('current-wallet').innerText = currentAmount;
+    document.getElementById('totalwithdraw').innerText = totalWithdraw;
+
+}
+
+function login(){
+    window.location.href = 'login.html';
+}
+function index(){
+    window.location.href = 'https://github.com/shariarfam';
+}
